442bfb90213ba60942f70695891514f4f893646d
Riccardo <riccardo.rossimori@studenti.unicam.it>
2020-12-07 16:18:28 +0100

    [NEW] Interface
    Created the new UtenteInterface to have two common methods for the actors Cliente (already implemented) and UNR (to implement)

---
+++ Code/src/UtenteInterface.java
@@ -0,0 +1,6 @@
+public interface UtenteInterface {
+    String ricercaNegozio (String neg);
+    String ricercaArticolo (String art);
+    /*TODO: Fare un parser di stringa "lato client" in modo da poterlo stampare
+            a video come una lista formattata.*/
+}
