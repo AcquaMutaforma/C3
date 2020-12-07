690aea47b065db53ae82a6e4ceb48e310620c9d8
Riccardo <riccardo.rossimori@studenti.unicam.it>
2020-12-07 16:22:25 +0100

    [ADJUSTMENT] Methods
    Added all the methods derivated from the implementation of the Interface "LockerInterface"

--- Code/src/Locker.java
+++ Code/src/Locker.java
@@ -1,2 +1,49 @@
-public class Locker {
+public class Locker implements LockerInterface{
+    /**
+     * Metodo assegnazione Box del locker ad un cliente
+     */
+    @Override
+    public void assegnaBox() {
+        
+    }
+
+    /**
+     * getter
+     *
+     * @param idbox
+     */
+    @Override
+    public BoxInterface getBox(int idbox) {
+        return null;
+    }
+
+    @Override
+    public BoxInterface getBox(BoxInterface b) {
+        return null;
+    }
+
+    /**
+     * Chiede al box inserito di generare una chiave e la ritorna
+     *
+     * @param idCliente
+     */
+    @Override
+    public int GetChiave(int idCliente) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getPosizione() {
+        return null;
+    }
 }
