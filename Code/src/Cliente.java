95588f6c6e5605442e32d3eb4f94503a87434814
Riccardo <riccardo.rossimori@studenti.unicam.it>
2020-12-07 16:21:25 +0100

    [ADJUSTMENT] Methods
    Added the methods derivated from the implementation of the Interface ClienteInterface.

--- Code/src/Cliente.java
+++ Code/src/Cliente.java
@@ -1,6 +1,39 @@
-public class Cliente {
+public class Cliente implements ClienteInterface{
 
     LockerInterface lock;
     BoxInterface box;
 
+    /**
+     * Collega Locker inserito, con il cliente
+     *
+     * @param s
+     */
+    @Override
+    public void setCheckpoint(String s) {
+
+    }
+
+    @Override
+    public int getCodiceCliente() {
+        return 0;
+    }
+
+    /**
+     * metodo temporaneo, va nella GUI
+     * Visualizza il locker che ha come checkpoint o nella lista Lockers
+     */
+    @Override
+    public BoxInterface visualizzaBox() {
+        return null;
+    }
+
+    @Override
+    public String ricercaNegozio(String neg) {
+        return null;
+    }
+
+    @Override
+    public String ricercaArticolo(String art) {
+        return null;
+    }
 }
