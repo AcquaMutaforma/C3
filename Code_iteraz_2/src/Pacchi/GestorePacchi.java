ddbc34f80b51eb06a896a9464f8faccb02f25ff7
Riccardo <riccardo.rossimori@studenti.unicam.it>
2021-01-13 20:20:10 +0100

    [Implemented] add/rm/set/get Methods
    Aggiunti metodi per le classi di gestione Pacchi e Corse.

--- Code_iteraz_2/src/Pacchi/GestorePacchi.java
+++ Code_iteraz_2/src/Pacchi/GestorePacchi.java
@@ -1,4 +1,29 @@
 package Pacchi;
 
+public class GestorePacchi implements GestorePacchiInterface {
+    @Override
+    public boolean addPacco(int idCommerciante, int idCliente) {
+        return creaPacco(idCommerciante,idCliente);
+    }
 
+    @Override
+    public boolean removePacco(int idPacco) {
+        Pacco p = getPacco(idPacco);
+        p=null;
+        return p==null;
+        //ho lasciato questa dicitura anziché semplificare poiché non é
+        // finita l'implementazione sopra | Riccardo
+    }
+
+    @Override
+    public Pacco getPacco(int idPacco) {
+        //come GestoreCorse.getCorsa | Riccardo
+        return null;
+    }
+
+    @Override
+    public boolean setPacco(int idPacco, int idCommerciante, int idCliente) {
+        Pacco p = getPacco(idPacco);
+        return p.setCommerciante(idCommerciante) && p.setCliente(idCliente);
+    }
 }
