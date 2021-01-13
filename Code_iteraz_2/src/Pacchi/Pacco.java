0ecf4e52aff5a0885581c39e3c040f983c6db723
Riccardo <riccardo.rossimori@studenti.unicam.it>
2021-01-13 20:07:49 +0100

    [Add] setMethods
    Aggiunti ed implementati i metodi di set per la classe Pacco.

--- Code_iteraz_2/src/Pacchi/Pacco.java
+++ Code_iteraz_2/src/Pacchi/Pacco.java
@@ -1,4 +1,15 @@
 package Pacchi;
 
 public class Pacco {
+    @Override
+    public boolean setCliente(int idCliente) {
+        this.idCliente=idCliente;
+        return true;
+    }
+
+    @Override
+    public boolean setCommerciante(int idCommerciante) {
+        this.idCommerciante=idCommerciante;
+        return true;
+    }
 }
