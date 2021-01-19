55d97d7980998df1d9979cbd7d80455e0f1506c7
Riccardo <riccardo.rossimori@studenti.unicam.it>
2021-01-19 19:31:38 +0100

    [Add] Methods and imports
    Aggiunti metodi da implementare che vengono chiamati dai gestori di pacchi e corse.
    Aggiunto inoltre un nuovo package per poter fare un import nei gestori della classe Sistema.

--- Code_iteraz_2/src/main/java/Sism/Sistema.java
+++ Code_iteraz_2/src/main/java/Sism/Sistema.java
@@ -1,7 +1,15 @@
+package Sism;//TODO cambiare nome al package... orrendo
+                //Lasciare il package o modificare gli import di conseguenza nelle varie classi.
+                // L'ho messo perché non mi faceva importare la classe in altro modo. | Riccardo
+import Corse.CorsaInterface;
 import Corse.GestoreCorseInterface;
 import Locker.GestoreLocker;
 import Pacchi.GestorePacchiInterface;
+import Pacchi.PaccoInterface;
 import Utenti.GestoreUtenti;
+
+import java.util.ArrayList;
+
 public class Sistema {
 
     private GestoreUtenti gestoreUtenti;
@@ -10,3 +18,29 @@ public class Sistema {
     //debba rimanere cosí o cambiare (mi riferisco ai gestori di pacchi e corse) | Riccardo
     private GestorePacchiInterface gestorePacchiInterface;
     private GestoreLocker gestoreLocker;
+    private static Sistema istanza;
+
+    private Sistema(){
+        //TODO Implementare costruttore privato, che viene invocato dal getInstance()
+    }
+
+    public static Sistema getInstance(){
+        //TODO Implementare metodo per avere un singleton
+        return istanza;
+    }
+
+    public int generaNuovoId() {
+        //TODO Implementare generatore Id
+        return 0;
+    }
+
+    public boolean savePacco(ArrayList<? extends PaccoInterface> listaPacchi) {
+        //TODO Implementare Saver per i pacchi
+        return true;
+    }
+
+    public boolean saveCorsa(ArrayList<? extends CorsaInterface> listaCorse) {
+        //TODO Implementare Saver per le corse
+        return true;
+    }
+}
