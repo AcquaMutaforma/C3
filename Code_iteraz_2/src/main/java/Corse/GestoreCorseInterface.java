78eb055f7e1d1c4a338dc4b1f263b7f08d574889
Riccardo <riccardo.rossimori@studenti.unicam.it>
2021-01-19 19:36:31 +0100

    [Add&Adjustment] Methods and import
    Aggiunto metodo di salvataggio corse.
    Aggiunto import per array dato come parametro nel metodo di cui sopra.
    Modificato il valore di ritorno del metodo creaCorsa con il valore di ritorno di conseguenza.

--- Code_iteraz_2/src/main/java/Corse/GestoreCorseInterface.java
+++ Code_iteraz_2/src/main/java/Corse/GestoreCorseInterface.java
@@ -1,13 +1,17 @@
 package Corse;
 
+import java.util.ArrayList;
+
 public interface GestoreCorseInterface {
 
     boolean addCorsa(int idCorriere, int idPacco);
     boolean removeCorsa(int codiceCorsa);
     CorsaInterface getCorsa(int codiceCorsa);
+    boolean saveCorsa(ArrayList<? extends CorsaInterface> listaCorse);
     //boolean setCorsa(int codiceCorsa, int idCorriere, int idPacco, int codiceLocker); //probabilmente non serve
-    default boolean creaCorsa(int idCorriere,int idPacco){
+    default Corsa creaCorsa(int idCorriere,int idPacco){
+        //TODO c'é un legame con la nostra implementazione, da togliere in qualche modo
         Corsa c = new Corsa(idCorriere,idPacco);
-        return c==null;
+        return c;
     }
 }
