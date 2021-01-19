9ee20b0edcdb34b9fd28d57b75c7becbdde4d789
Riccardo <riccardo.rossimori@studenti.unicam.it>
2021-01-19 19:32:08 +0100

    Update Corsa.java

--- Code_iteraz_2/src/main/java/Corse/Corsa.java
+++ Code_iteraz_2/src/main/java/Corse/Corsa.java
@@ -2,31 +2,31 @@ package Corse;
 
 public class Corsa implements CorsaInterface{
 
-    int idCorriere, idPacco, codiceLocker;
+    int idCorriere, idPacco, codiceLocker, idCorsa;
     //TODO nel costruttore forse andrebbe aggiunto un Locker, altrimenti rimane senza destinazione
 
     public Corsa(int idCorriere, int idPacco) {
-        setCorriere(idCorriere);
+        setCorriere(idCorriere);//TODO e se questi setters fossero privati?
         setPacco(idPacco);
     }
 
     @Override
     public boolean setCorriere(int idCorriere) {
-        //TODO opzione false
+        //TODO opzione false o diventa void?
         this.idCorriere=idCorriere;
         return true;
     }
 
     @Override
     public boolean setCodice(int codiceLocker) {
-        //TODO opzione false
+        //TODO opzione false o diventa void?
         this.codiceLocker=codiceLocker;
         return true;
     }
 
     @Override
     public boolean setPacco(int idPacco) {
-        //TODO opzione false
+        //TODO opzione false o diventa void?
         this.idPacco=idPacco;
         return true;
     }
