0112f7e3ae52975f07b4e0e0f067bad862bc979b
Urolit <aley.pallotta@gmail.com>
2021-01-26 18:08:46 +0100

    Solo spostamenti
    Ho spostato tutti i nuovi file in un "progetto springboot". Nessuna modifica al codice, per problemi strani ho i backup

--- Code_iteraz_2/src/main/java/it/arrp/c3/ConnessioneDB.java
+++ Code_iteraz_2/src/main/java/it/arrp/c3/ConnessioneDB.java
@@ -13,7 +13,6 @@ public class ConnessioneDB{
     public DataSource getDataSource() throws URISyntaxException {
 
         URI dbUri = new URI(System.getenv("    postgres://wfkvealbwjaocc:f02f25dded729de358f9537669abf4e1add605e1286959e074e560c8b2c5e634@ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/d3b7ckr2egg9as"));
-
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
