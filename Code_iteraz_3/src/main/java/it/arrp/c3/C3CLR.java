75742533e0149e4ab53791ec8e7f04126238575f
Urolit <aley.pallotta@gmail.com>
2021-05-12 16:47:06 +0200

    [CODE] Small fixes per connettersi
    Ho tolto alcune parti commentate in application.properties e ho aggiunto qualcosa per testare in C3CLR

--- Code_iteraz_3/src/main/java/it/arrp/c3/C3CLR.java
+++ Code_iteraz_3/src/main/java/it/arrp/c3/C3CLR.java
@@ -1,14 +1,25 @@
 package it.arrp.c3;
 
+import it.arrp.c3.Model.Cliente;
+import it.arrp.c3.Model.Repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 /**
  * C3 Command Line Runner
  */
+@Component
 public class C3CLR implements CommandLineRunner {
 
+    @Autowired
+    ClienteRepository clienteRepository;
+
     @Override
-    public void run(String... args) throws Exception {
-        //TODO
+    public void run(String... args){
+        Cliente sys = new Cliente("system", "email", "admin", "Camerino");
+        sys.setId(0L);
+        clienteRepository.save(sys);
+
     }
 }
